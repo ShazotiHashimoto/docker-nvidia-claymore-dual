089e90760975cfeebe2e314fc2149a0ee2b0e01f
Shahzaad Tayob <shahzaadt@gmail.com>
2020-05-10 18:41:50 +0200

    add fiels

--- start.bash
+++ start.bash
@@ -1,2 +1,2 @@
 !#/bin/bash
-./ethdcrminer64 -epool stratum+tcp://daggerhashimoto.eu.nicehash.com:3353 -ewal 347i1EdYDG393J2XwRgB3mnw5HsyqsEW9N -eworker 347i1EdYDG393J2XwRgB3mnw5HsyqsEW9N.YourWorkerName -epsw x -esm 3 -allpools 1 -dpool stratum+tcp://sia.eu.nicehash.com:3360 -dwal 347i1EdYDG393J2XwRgB3mnw5HsyqsEW9N.w2 -dpsw x -dcoin sia
+./ethdcrminer64 -epool stratum+tcp://daggerhashimoto.usa.nicehash.com:3353 -ewal 347i1EdYDG393J2XwRgB3mnw5HsyqsEW9N -eworker 347i1EdYDG393J2XwRgB3mnw5HsyqsEW9N.YourWorkerName -epsw x -esm 3 -allpools 1 -dpool stratum+tcp://cuckaroom.usa.nicehash.com:3382 -dwal 347i1EdYDG393J2XwRgB3mnw5HsyqsEW9N.w2 -dpsw x
