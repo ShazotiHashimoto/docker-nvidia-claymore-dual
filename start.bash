de219e973a6662d19e0a774f417ae862d5d8679e
Shahzaad Tayob <shahzaadt@gmail.com>
2020-05-10 19:28:35 +0200

    updateconfig

--- start.bash
+++ start.bash
@@ -1,2 +1,2 @@
 !#/bin/bash
-./ethdcrminer64 -epool stratum+tcp://daggerhashimoto.eu-new.nicehash.com:3353 -ewal 347i1EdYDG393J2XwRgB3mnw5HsyqsEW9N -epsw x -dbg -1 -retrydelay 1 -ftime 55 -tt 79 -ttli 77 -tstop 89 -tstart 85 -fanmin 30 -r 0 -esm 3 -estale 0 -erate 1 -allpools 1
+./ethdcrminer64 -epool stratum+tcp://daggerhashimoto.eu-new.nicehash.com:3353 -ewal 347i1EdYDG393J2XwRgB3mnw5HsyqsEW9N -epsw x -dbg -1 -retrydelay 1 -ftime 55 -tt 79 -ttli 77 -tstop 89 -tstart 85 -fanmin 30 -r 0 -esm 3 -estale 0 -erate 1
