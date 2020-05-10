6b9bf49f9133b3d70f962d26a191236e1f90e4fa
Shahzaad Tayob <shahzaadt@gmail.com>
2020-05-11 00:03:42 +0200

    fix wallet code

--- 15. Ethereum-nicehash.bash
+++ 15. Ethereum-nicehash.bash
@@ -1,3 +1,3 @@
-./ethdcrminer64 -epool stratum+tcp://daggerhashimoto.usa.nicehash.com:3353 -ewal 1Pz3EcHsRuJkraNRACUrBrLaxiTPP1VHJZ -epsw x -dbg -1 -retrydelay 1 -ftime 55 -tt 79 -ttli 77 -tstop 89 -tstart 85 -fanmin 30 -r 0 -esm 3 -estale 0 -erate 1 -allpools 1 -colors 0
+./ethdcrminer64 -epool stratum+tcp://daggerhashimoto.usa.nicehash.com:3353 -ewal 347i1EdYDG393J2XwRgB3mnw5HsyqsEW9N -epsw x -dbg -1 -retrydelay 1 -ftime 55 -tt 79 -ttli 77 -tstop 89 -tstart 85 -fanmin 30 -r 0 -esm 3 -estale 0 -erate 1 -allpools 1 -colors 0
 
 
