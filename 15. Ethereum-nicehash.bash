54cf9910b8ce553e849690b0d86aa8cbf5ed49cd
Shahzaad Tayob <shahzaadt@gmail.com>
2020-05-10 23:33:03 +0200

    remove colors

--- 15. Ethereum-nicehash.bash
+++ 15. Ethereum-nicehash.bash
@@ -1,3 +1,3 @@
-./ethdcrminer64 -epool stratum+tcp://daggerhashimoto.usa.nicehash.com:3353 -ewal 1Pz3EcHsRuJkraNRACUrBrLaxiTPP1VHJZ -epsw x -dbg -1 -retrydelay 1 -ftime 55 -tt 79 -ttli 77 -tstop 89 -tstart 85 -fanmin 30 -r 0 -esm 3 -estale 0 -erate 1 -allpools 1
+./ethdcrminer64 -epool stratum+tcp://daggerhashimoto.usa.nicehash.com:3353 -ewal 1Pz3EcHsRuJkraNRACUrBrLaxiTPP1VHJZ -epsw x -dbg -1 -retrydelay 1 -ftime 55 -tt 79 -ttli 77 -tstop 89 -tstart 85 -fanmin 30 -r 0 -esm 3 -estale 0 -erate 1 -allpools 1 -colors 0
 
 
